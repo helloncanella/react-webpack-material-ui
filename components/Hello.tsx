ee8c83967f9183c4c6bc6900a46e1580f0e2ec13
Hellon Canella <helloncanella@gmail.com>
2016-08-22 20:30:16 -0300

    starting material-ui

--- components/Hello.tsx
+++ components/Hello.tsx
@@ -5,5 +5,5 @@ export interface HelloProps { compiler: string; framework: string; }
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
         return <h1> {this.props.compiler} and {this.props.framework}!</h1>;
-    }
-}  
+     } 
+}   
